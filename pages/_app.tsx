967f3572164cb4223ac015d841bff034775b0c1c
Daniel Hochman <danielhochman@users.noreply.github.com>
2023-08-06 20:05:15 -0500

    basic search of tv shows (#1)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+import { Open_Sans } from "next/font/google";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const openSans = Open_Sans({ subsets: ["latin"] });
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+  }
+});
+
+const theme = extendTheme({
+  fonts: {
+    body: openSans.style.fontFamily,
+    heading: openSans.style.fontFamily,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
 }
